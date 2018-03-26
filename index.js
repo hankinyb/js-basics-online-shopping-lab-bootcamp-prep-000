50f1a972e1dfb92ecc26bb987fafe4b61c861e25
Yan Hankin <hankinyb@gmail.com>
2018-03-26 22:49:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,11 +65,19 @@ function removeFromCart(item) {
       cart.splice(i, 1)
       return cart
     } 
-  }
+  } 
   console.log('That item is not in your cart.')
   return cart;
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if(cardNumber) {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    for (var i = cart.length; i>= 0; i--) {
+      cart.pop();
+    } 
+    } else 
+      console.log('Sorry, we don\'t have a credit card on file for you.')
+return cart;
+  
 }
